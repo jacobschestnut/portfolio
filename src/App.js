c65c317ebaf820d742facfd0f1ed1294b221a648
Jake Chestnut <jacobschestnut@gmail.com>
2022-07-26 17:29:59 -0500

    got columns and simple navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-function App() {
+import './App.scss'
+
+function BasicExample() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container >
+
+      <Navbar bg="light" expand="sm" fixed="top" variant="light" >
+        <Container fluid>
+          <Navbar.Brand href="/home">Jacob Chestnut</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/home">Home</Nav.Link>
+              <Nav.Link href="/contact">Contact</Nav.Link>
+              <NavDropdown title="Projects" id="basic-nav-dropdown">
+                <NavDropdown.Item href="/nice-tapes">Nice Tapes</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <Row className="border">
+        <Col className="border d-flex justify-content-right">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Carter_vs_Gasol%2C_Lakers_vs_Magic.jpg/275px-Carter_vs_Gasol%2C_Lakers_vs_Magic.jpg"/>
+        </Col>
+        <Col className="border d-flex justify-content-left">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Carter_vs_Gasol%2C_Lakers_vs_Magic.jpg/275px-Carter_vs_Gasol%2C_Lakers_vs_Magic.jpg"/>
+        </Col>
+      </Row>
+
+    </Container>
   );
 }
 
-export default App;
+export default BasicExample;
